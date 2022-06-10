3c9257e6cb3926b8143525b4182bc64d0f1ff3a1
Kelvin <vokemalosh32@gmail.com>
2022-06-10 18:39:56 +0300

    commit tree c80db837cad3a52c995ad35d0b2de790b3859f39

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+
+ function superbowlWin(findWin){
+     const yearWin = findWin.find(win => win.result === "W")
+ //return yearWin ? yearWin.yearWin : undefined
+ if(yearWin){
+    return yearWin.year
+ }else{
+    return undefined
+ }
+
+ }
